9211e777c1884183011e1a24d0f313486892e18f
PizzaDragon <hann0009@hz.nl>
2019-09-17 15:02:53 +0200

    eindopdracht 17-09

---
+++ types-&-variables/assets/app.js
@@ -0,0 +1,11 @@
+let x = 2;
+let y = 13;
+console.log(y % x);
+
+let message = 'Programming is not so cool';
+console.log(message.replace(" not ", " "));
+
+const a = 1400;
+const b = 'Ik woon in Naboo';
+console.log(a < b);
+//je vergelijkt een string met een value, de uitkomst zal dan altijd false zijn
